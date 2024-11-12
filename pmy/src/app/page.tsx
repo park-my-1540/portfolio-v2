b2011a18c15b0732eda4d72cc10fdcad4d28a3f7
sia <parkmy722698@gmail.com>
2024-11-12 13:32:17 +0900

    Init: porftolio - v2

---
+++ pmy/src/app/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+import { test } from "@/app/styles/style.css";
+export default function Home() {
+  return (
+    <div className={test}>
+      ddd
+    </div>
+  );
+}
---
+++ pmy/src/app/styles/globals.css.ts
@@ -0,0 +1,44 @@
+// global.css.ts
+import "./layers.css";
+import "./reset.css";
+
+import { globalStyle } from "@vanilla-extract/css";
+import * as layers from "./layers.css";
+
+globalStyle(
+  "#__next",
+  {
+    "@layer": {
+      [layers.reset]: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100%"
+      },
+    },
+  }
+);
+
+globalStyle("body, html", {
+  "@layer": {
+    [layers.reset]: {
+      height: "100%",
+    },
+  },
+});
+
+globalStyle("button", {
+  "@layer": {
+    [layers.reset]: {
+      cursor: "pointer",
+      transition: "opacity 0.3s ease", // 오퍼시티 전환 효과
+    },
+  },
+});
+
+// 비활성화된 버튼 스타일
+globalStyle("button:disabled", {
+  opacity: 0.5, // 비활성화된 버튼의 오퍼시티
+  cursor: "not-allowed", // 비활성화된 상태에서 커서 스타일 변경
+});
