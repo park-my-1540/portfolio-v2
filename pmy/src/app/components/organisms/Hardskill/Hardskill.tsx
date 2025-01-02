1cda6bb47afdefa0dc4a8c870c34ea7268561559
sia <parkmy722698@gmail.com>
2025-01-03 00:07:41 +0900

    fix. 안쓰는 컴포넌트 삭제

--- pmy/src/app/components/organisms/Hardskill/Hardskill.tsx
+++ pmy/src/app/components/organisms/Hardskill/Hardskill.tsx
@@ -1,38 +1,35 @@
-import React from "react";
-import { Text } from "@/components/atoms/Text/Text";
-import { CardImages } from "@/components/molecules/Card/Card";
+import React from 'react';
+import { Text } from '@/components/atoms/Text/Text';
 
 const hardskills = [
   {
     type: 'react',
-    url: './svg/hardskill/react.svg'
+    url: './svg/hardskill/react.svg',
   },
   {
     type: 'javascript',
-    url: './svg/hardskill/javascript.svg'
+    url: './svg/hardskill/javascript.svg',
   },
   {
     type: 'angularjs',
-    url: './svg/hardskill/angularjs.svg'
+    url: './svg/hardskill/angularjs.svg',
   },
   {
     type: 'html',
-    url: './svg/hardskill/html.svg'
+    url: './svg/hardskill/html.svg',
   },
   {
     type: 'css',
-    url: './svg/hardskill/css.svg'
+    url: './svg/hardskill/css.svg',
   },
-]
+];
 
 export default function HardSkill() {
-    return (
-        <>
-          <Text sizes="largeX2" weights="bold">HardSkill</Text>
-          <CardImages
-            theme="dark"
-            hardskills={hardskills}
-          />
-        </>
-      );
+  return (
+    <>
+      <Text sizes="largeX2" weights="bold">
+        HardSkill
+      </Text>
+    </>
+  );
 }
--- pmy/src/app/components/pages/ResumePage.tsx
+++
@@ -1,5 +0,0 @@
-import Resume from "@/components/templates/Resume";
-
-export default function ResumePage() {
-  return <Resume/>;
-}
