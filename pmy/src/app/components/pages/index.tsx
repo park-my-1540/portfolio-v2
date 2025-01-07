7e6730e52f2b4d172b0f9dc4d6d71bb7ed8d4451
sia <parkmy722698@gmail.com>
2025-01-07 21:25:05 +0900

    Revert "임시커밋2"
    This reverts commit c6b0db98cd4de79431670075daa988d1d1cdacf3.

--- pmy/src/app/components/pages/index.tsx
+++ pmy/src/app/components/pages/index.tsx
@@ -1,102 +1,30 @@
-import React, { useEffect, useRef } from 'react';
-import LocomotiveScroll from 'locomotive-scroll';
+import React, { useRef, useState, useEffect } from 'react';
+import useLocoScroll from '@/utils/hook/useLocoScroll';
 
 import MainPage from './MainPage';
 import AboutPage from './AboutPage';
 import ProjectsPage from '../../../pages/projects';
-import ScrollTrigger from 'gsap/ScrollTrigger';
-import gsap from 'gsap';
-import * as animate from '@/utils/animate';
 
-import Gallery from '@/components/organisms/Gallery/demo';
+import { useSetAtom } from 'jotai';
+import { viewState } from '@/jotai/viewAtom';
 
-gsap.registerPlugin(ScrollTrigger);
+const ScrollBox: React.FC = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [preloader, setPreload] = useState(false);
+  const setContainerRef = useSetAtom(viewState);
 
-const Demo: React.FC = () => {
-  const ref = useRef(null);
-  const scrollContainerRef = useRef<HTMLDivElement>(null);
-
+  useLocoScroll(!preloader, containerRef);
   useEffect(() => {
-    const scrollEl = document.querySelector('#main-container');
-    const locoScroll = new LocomotiveScroll({
-      el: scrollEl,
-      smooth: true,
-      multiplier: 1,
-      class: 'is-reveal',
-      // tablet: {
-      //   smooth: true,
-      //   direction: 'vertical', // 기본 방향 세로
-      // },
-      // smartphone: {
-      //   smooth: true,
-      //   direction: 'vertical',
-      // },
-    });
-
-    locoScroll.on('scroll', () => {
-      ScrollTrigger.update();
-      // setScrollState({ scrollStart: true });
-    });
-
-    ScrollTrigger.scrollerProxy(scrollEl, {
-      scrollTop(value) {
-        if (locoScroll) {
-          return arguments.length
-            ? locoScroll.scrollTo(value, 0, 0)
-            : locoScroll.scroll.instance.scroll.y;
-        }
-        return null;
-      },
-      scrollLeft(value) {
-        if (locoScroll) {
-          return arguments.length
-            ? locoScroll.scrollTo(value, 0, 0)
-            : locoScroll.scroll.instance.scroll.x;
-        }
-        return null;
-      },
-      getBoundingClientRect() {
-        return {
-          left: 0,
-          top: 0,
-          width: window.innerWidth,
-          height: window.innerHeight,
-        };
-      },
-      pinType: scrollEl.style.transform ? 'transform' : 'fixed',
-    });
-
-    const lsUpdate = () => {
-      if (locoScroll) {
-        locoScroll.update();
-      }
-    };
-
-    ScrollTrigger.addEventListener('refresh', lsUpdate);
-    ScrollTrigger.refresh();
-
-    return () => {
-      if (scroll) scroll.destroy();
-    };
+    setContainerRef({ containerRef });
   }, []);
-
-  let timer: any;
-
   return (
-    <div data-scroll-container id="main-container">
-      <div className="vertical-scroll" data-scroll-section>
-        <h1 data-scroll>Vertical Section</h1>
-        <p data-scroll>Scroll down to see more...</p>
-      </div>
-
-      <Gallery />
-
-      <div className="vertical-scroll" data-scroll-section>
-        <h1 data-scroll>Another Vertical Section</h1>
-        <p data-scroll>Scroll more to explore!</p>
-      </div>
+    <div className="smooth-scroll" id="main-container" ref={containerRef}>
+      <MainPage />
+      <AboutPage />
+      <ProjectsPage />
+      <section className="panel gray">DONE!</section>
     </div>
   );
 };
 
-export default Demo;
+export default ScrollBox;
