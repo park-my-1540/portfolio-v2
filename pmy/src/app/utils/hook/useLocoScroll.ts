ece5a19b61b0d72b25447f440563003a7706a5af
sia <parkmy722698@gmail.com>
2025-01-08 13:50:19 +0900

    Fix, router change 될때 pin 고정한 영역 어그러지는 현상.... [해결] scrollTrigger timeline을 ref로 저장 후 pathname 변경 될때 이 scrollTrigger 인스턴스만 새로고침

--- pmy/src/app/utils/hook/useLocoScroll.ts
+++ pmy/src/app/utils/hook/useLocoScroll.ts
@@ -1,4 +1,5 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
+import { usePathname } from 'next/navigation';
 import { useSetAtom } from 'jotai';
 import { viewState } from '@/jotai/viewAtom';
 import ScrollTrigger from 'gsap/ScrollTrigger';
@@ -15,94 +16,122 @@ gsap.registerPlugin(ScrollTrigger);
  */
 export default function useLocoScroll(start: boolean, ref: any) {
   const setScrollState = useSetAtom(viewState);
+  const pathname = usePathname(); // 현재 경로
+  const locoScrollRef = useRef<LocomotiveScroll | null>(null);
+  const tl = useRef<gsap.core.Timeline | null>(null);
 
   useEffect(() => {
     if (!ref.current) return;
 
     const initializeLocoScroll = async () => {
       const { default: LocomotiveScroll } = await import('locomotive-scroll');
-      const gsap = (await import('gsap')).default;
-      const ScrollTrigger = (await import('gsap/ScrollTrigger')).default;
+      const { default: gsap } = await import('gsap');
+      const { default: ScrollTrigger } = await import('gsap/ScrollTrigger');
+
       gsap.registerPlugin(ScrollTrigger);
+
       const scrollEl = ref.current as HTMLElement | null;
-
       if (!scrollEl) return;
-      const locoScroll = new LocomotiveScroll({
-        el: scrollEl,
-        smooth: true,
-        multiplier: 1,
-        class: 'is-reveal',
-      });
 
-      locoScroll.on('scroll', () => {
-        ScrollTrigger.update();
-      });
+      // 기존 locoScroll 인스턴스가 있는 경우 초기화 방지
+      if (!locoScrollRef.current) {
+        locoScrollRef.current = new LocomotiveScroll({
+          el: scrollEl,
+          smooth: true,
+          multiplier: 1,
+          class: 'is-reveal',
+        });
 
-      // ScrollTrigger와 Locomotive Scroll 동기화
-      ScrollTrigger.scrollerProxy(ref.current, {
-        scrollTop(value) {
-          if (locoScroll) {
-            return arguments.length
-              ? null
-              : locoScroll.scroll.instance.scroll.y;
-          }
-          return null;
-        },
-        scrollLeft(value) {
-          if (locoScroll) {
-            return arguments.length
-              ? null
-              : locoScroll.scroll.instance.scroll.x;
-          }
-          return null;
-        },
-        getBoundingClientRect() {
-          return {
-            left: 0,
-            top: 0,
-            width: window.innerWidth,
-            height: window.innerHeight,
-          };
-        },
-      });
+        locoScrollRef.current.on('scroll', () => {
+          ScrollTrigger.update();
+        });
 
-      const lsUpdate = () => {
-        if (locoScroll) {
-          locoScroll.update();
+        // ScrollTrigger와 Locomotive Scroll 동기화
+        ScrollTrigger.scrollerProxy(scrollEl, {
+          scrollTop(value) {
+            if (locoScrollRef.current) {
+              return arguments.length
+                ? null
+                : locoScrollRef.current.scroll.instance.scroll.y;
+            }
+            return null;
+          },
+          scrollLeft(value) {
+            if (locoScrollRef.current) {
+              return arguments.length
+                ? null
+                : locoScrollRef.current.scroll.instance.scroll.x;
+            }
+            return null;
+          },
+          getBoundingClientRect() {
+            return {
+              left: 0,
+              top: 0,
+              width: window.innerWidth,
+              height: window.innerHeight,
+            };
+          },
+        });
+
+        // 갤러리 애니메이션 설정
+        const galleryEl = document.querySelector('.gallery') as HTMLElement;
+        const sections = gsap.utils.toArray('.gallery-item-wrapper');
+
+        if (galleryEl) {
+          tl.current = gsap.timeline({
+            scrollTrigger: {
+              start: 'top top',
+              trigger: galleryEl,
+              scroller: '#main-container',
+              end: () => `+=${galleryEl.offsetWidth}`,
+              pin: true,
+              scrub: 0.5,
+              snap: 1 / (sections.length - 1),
+            },
+          });
+
+          tl.current.to(sections, {
+            xPercent: -100 * (sections.length - 1),
+            ease: 'none',
+          });
         }
-      };
 
-      //gallery animation
-      const galleryEl = document.querySelector('.gallery') as HTMLElement; // HTMLElement로 타입 캐스팅
-      const sections = gsap.utils.toArray('.gallery-item-wrapper');
+        // ScrollTrigger 새로고침
+        const refreshScrollTrigger = () => {
+          if (locoScrollRef.current) {
+            locoScrollRef.current.update();
+          }
+        };
 
-      if (!galleryEl) return;
-      gsap.to(sections, {
-        xPercent: -100 * (sections.length - 1),
-        ease: 'none',
-        scrollTrigger: {
-          start: 'top top',
-          trigger: galleryEl,
-          scroller: '#main-container',
-          end: () => `+=${galleryEl.offsetWidth}`,
-          pin: true,
-          scrub: 0.5,
-          snap: 1 / (sections.length - 1),
-        },
-      });
-
-      ScrollTrigger.addEventListener('refresh', lsUpdate);
-      ScrollTrigger.refresh();
-
-      return () => {
-        if (locoScroll) {
-          ScrollTrigger.removeEventListener('refresh', lsUpdate);
-          locoScroll.destroy();
-          console.log('Kill', locoScroll);
-        }
-      };
+        ScrollTrigger.addEventListener('refresh', refreshScrollTrigger);
+        ScrollTrigger.refresh();
+      }
     };
 
     initializeLocoScroll();
+
+    return () => {
+      // ScrollTrigger와 Locomotive Scroll 정리
+      if (locoScrollRef.current) {
+        ScrollTrigger.removeEventListener('refresh', () => {
+          initializeLocoScroll();
+        });
+        locoScrollRef.current.destroy();
+        locoScrollRef.current = null;
+      }
+
+      if (tl.current) {
+        tl.current.kill();
+        tl.current = null;
+      }
+    };
   }, [ref]);
+
+  // 경로 변경 시 ScrollTrigger 인스턴스 새로고침
+  useEffect(() => {
+    if (tl.current && tl.current.scrollTrigger) {
+      tl.current.scrollTrigger.refresh();
+    }
+  }, [pathname]);
 }
