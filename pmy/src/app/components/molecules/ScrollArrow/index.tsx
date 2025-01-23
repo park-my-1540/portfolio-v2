914828c826525150101837f0286e8e2a90305f8f
sia <parkmy722698@gmail.com>
2025-01-23 22:45:45 +0900

    Fix. main - svg 이미지 더 큰거로 교체하면서 svg 이미지 크기에 맞게 bounding크기 일치시키도록 scale 값 계산 - fallingObj svg로 교체 - fallingObj 1.5초 마다 5개씩 떨어지도록 변경

--- pmy/src/app/components/molecules/ScrollArrow/index.tsx
+++ pmy/src/app/components/molecules/ScrollArrow/index.tsx
@@ -1,8 +1,10 @@
 import './style.css';
+import { Text } from '@/components/atoms/Text/Text';
 export default function ScrollArrow() {
   return (
     <div className="scroll-down">
-      Scroll down<div className="arrow"></div>
+      <Text color="tertiary">Scroll down</Text>
+      <div className="arrow"></div>
     </div>
   );
 }
--- pmy/src/app/components/organisms/Main/Matter/Matter.tsx
+++ pmy/src/app/components/organisms/Main/Matter/Matter.tsx
@@ -50,8 +50,8 @@ const MatterMain: React.FC = () => {
     const floor = Matter.Bodies.rectangle(
       bounding.left + bounding.width / 2,
       bounding.top + bounding.height + 5 + 10,
-      bounding.width,
-      10,
+      bounding.width / 1.5,
+      400,
       {
         isStatic: true,
         collisionFilter: {
@@ -63,65 +63,115 @@ const MatterMain: React.FC = () => {
         },
       },
     );
+
     Matter.Composite.add(world, floor);
     engine.world.gravity.y = 2; // 기본값은 1. 3으로 설정하여 더 빠르게.
+
     // Dropping objects
     let timer: ReturnType<typeof setTimeout>;
-    const createFallingObjectsWithDelay = () => {
-      const objectCount = 15; // 생성할 객체 수
-      const delay = 100; // 각 객체 생성 간 시간차 (ms)
-      const startingHeight = -100; // 객체가 생성될 초기 Y 좌표 (화면 상단 위)
-      for (let i = 0; i < objectCount; i++) {
-        timer = setTimeout(() => {
-          if (!canvasBoxRef.current) return;
-          const randomX =
-            Math.random() * (canvasBoxRef.current?.offsetWidth - 500); // 화면 내 무작위 위치
-          const rectangle = Matter.Bodies.circle(randomX, startingHeight, 30, {
-            density: 0.05, // 기본값은 0.001, 더 큰 값으로 설정
-            frictionAir: 0.01, // 공기 저항 유지
-            restitution: 0.3, // 반발력 설정 (충돌 후 튀는 정도)
-            render: {
-              fillStyle: '#219B9D',
-            },
-            collisionFilter: {
-              category: 0x0004,
-            },
-          });
 
-          Matter.Composite.add(world, rectangle);
-        }, i * delay); // `i * delay`만큼 시간차 적용
-      }
-      clearTimeout(timer);
+    const createFallingObjects = () => {
+      const texturePath = './svg/main/lightning.svg';
+      const image = new Image();
+      image.src = texturePath;
+
+      const totalObjectCount = 20; // 전체 생성할 객체 수
+      const groupSize = 6;
+      const delay = 100; // 각 객체 생성 간 시간차
+      const startingHeight = -100; // 객체가 생성될 초기 Y
+
+      let createdObjects = 0; // 현재까지 생성된 객체 수
+
+      const createObjects = () => {
+        for (let i = 0; i < groupSize; i++) {
+          if (createdObjects >= totalObjectCount) {
+            clearInterval(interval);
+            break;
+          }
+
+          timer = setTimeout(() => {
+            if (!canvasBoxRef.current) return;
+            const randomX =
+              Math.random() * (canvasBoxRef.current?.offsetWidth - 500); // 화면 내 무작위 위치
+            const randomAngle = Math.random() * Math.PI * 2; // 0 ~ 2π (0도 ~ 360도) 랜덤 각도
+
+            const rectangle = Matter.Bodies.rectangle(
+              randomX,
+              startingHeight,
+              56,
+              15,
+              {
+                density: 0.5, // 밀도
+                frictionAir: 0.01, // 공기 저항
+                restitution: 0.4, // 반발력
+                render: {
+                  sprite: {
+                    texture: texturePath,
+                    xScale: 1,
+                    yScale: 1,
+                  },
+                },
+              },
+            );
+
+            Matter.Body.setAngle(rectangle, randomAngle);
+            Matter.Composite.add(world, rectangle);
+          }, i * delay); // 그룹 내 객체 간 시간차
+        }
+        clearTimeout(timer);
+        createdObjects += groupSize;
+
+        if (createdObjects >= totalObjectCount) {
+          clearInterval(interval);
+        }
+      };
+
+      // 처음에 즉시 실행
+      createObjects();
+
+      // 이후 일정 시간마다 실행
+      const interval = setInterval(createObjects, 1500);
     };
 
-    createFallingObjectsWithDelay();
+    createFallingObjects();
 
     const paths = titleRef.current?.querySelectorAll('path');
 
     paths?.forEach((path) => {
       const letter = path.getAttribute('data-letter');
       const bounding = path.getBoundingClientRect();
-      const rectangle = Matter.Bodies.rectangle(
-        bounding.left + bounding.width / 2 - 30,
-        bounding.top + bounding.height / 2 - 120,
-        bounding.width,
-        bounding.height + 20,
-        {
-          isSleeping: true,
-          collisionFilter: {
-            category: 0x0004,
-          },
-          render: {
-            sprite: {
-              texture: `./svg/main/letter-${letter}.svg`,
-              xScale: 1,
-              yScale: 1,
+
+      // 이미지 크기와 bounding 크기를 일치시키기 위해 스케일 계산
+      const texturePath = `./svg/main/letter-${letter}.svg`;
+      const image = new Image();
+      image.src = texturePath;
+
+      image.onload = () => {
+        const xScale = bounding.width / image.width;
+        const yScale = bounding.height / image.height;
+
+        const rectangle = Matter.Bodies.rectangle(
+          bounding.left + bounding.width / 2 - 30,
+          bounding.top + bounding.height / 2 - 120,
+          bounding.width,
+          bounding.height + 20,
+          {
+            isSleeping: true,
+            collisionFilter: {
+              category: 0x0004,
+            },
+            render: {
+              sprite: {
+                texture: texturePath,
+                xScale: xScale,
+                yScale: yScale,
+              },
             },
           },
-        },
-      );
+        );
 
-      Matter.Composite.add(world, rectangle);
+        Matter.Composite.add(world, rectangle);
+      };
     });
 
     return () => {
