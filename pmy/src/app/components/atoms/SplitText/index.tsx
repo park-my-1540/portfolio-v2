0324de536f85b70851f8aceb54ab157bcaea3b25
sia <parkmy722698@gmail.com>
2025-01-24 20:34:36 +0900

    Fix. header, splitText

--- pmy/src/app/components/atoms/SplitText/index.tsx
+++ pmy/src/app/components/atoms/SplitText/index.tsx
@@ -1,7 +1,10 @@
+import { useEffect } from 'react';
 import './animate.css';
 import cn from 'classnames';
 import { textColor, TextColorVariantProps } from '@/styles/common/theme.css';
 import { text, TextVariantProps } from './splitText.module.css';
+import Splitting from 'splitting';
+import 'splitting/dist/splitting.css';
 
 type SplitTextProps = {
   splitText: string;
@@ -20,9 +23,12 @@ export const SplitText = ({
   type = 'txt',
   onClick,
 }: SplitTextProps & Partial<TextVariantProps & TextColorVariantProps>) => {
+  useEffect(() => {
+    Splitting();
+  }, []);
   const linkUrl = url ? url : 'javascript:void(0)';
   return (
-    <a href={linkUrl} className={text({ sizes })} onClick={onClick}>
+    <a href={linkUrl} className={cn('link', text({ sizes }))} onClick={onClick}>
       {type !== 'same' && (
         <p
           className={cn(
--- pmy/src/app/components/organisms/Header/header.css.ts
+++ pmy/src/app/components/organisms/Header/header.css.ts
@@ -7,18 +7,6 @@ export const header = style({
   width: '100%',
   zIndex: 99,
   color: 'inherit',
-  selectors: {
-    '&::before': {
-      content: '',
-      display: 'block',
-      width: 'calc(100% - 5px)',
-      height: 50,
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      backgroundColor: vars.color.light,
-    },
-  },
 });
 
 export const inner = style({
@@ -59,4 +47,16 @@ export const menuBtn = style({
 export const sub = style({
   border: '2px solid',
   background: vars.color.light,
+  selectors: {
+    '&::before': {
+      content: '',
+      display: 'block',
+      width: 'calc(100% - 5px)',
+      height: 50,
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      backgroundColor: vars.color.light,
+    },
+  },
 });
