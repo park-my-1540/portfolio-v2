098614b9adb1c9a0056baafc95d2b086c6a6c608
sia <parkmy722698@gmail.com>
2025-02-03 21:03:51 +0900

    Refactor. change component name MainSwiper -> ImageSwiper delete swiper.css.ts

---
+++ pmy/src/app/components/organisms/Main/Swiper/ImageSwiper.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Mousewheel,
+} from 'swiper/modules';
+import { SwiperProps } from '@/types/common';
+
+export default function ImageSwiper({
+  swiperRef,
+  pages,
+  onSlideChange,
+}: SwiperProps) {
+  return (
+    <Swiper
+      direction={'vertical'}
+      speed={1000}
+      slidesPerView={1}
+      spaceBetween={5}
+      modules={[Navigation, Pagination, Scrollbar, A11y, Mousewheel]}
+      navigation={{
+        nextEl: '.main-next',
+        prevEl: '.main-prev',
+      }}
+      scrollbar={{ draggable: true }}
+      onSlideChange={(swiper) => onSlideChange(swiper.realIndex)}
+      mousewheel={true}
+      keyboard={true}
+      ref={swiperRef}
+      pagination={{
+        el: '.main-pagination',
+        clickable: true,
+        renderBullet: function (index, className) {
+          return `<button type="button" class="${className} ${bullet}"></button>`;
+        },
+      }}
+    >
+      {pages.map((page, index) => (
+        <SwiperSlide key={index}>{page}</SwiperSlide>
+      ))}
+    </Swiper>
+  );
+}
