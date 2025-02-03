f8721cb420a4d4f839f44c28da636d3814122d2d
sia <parkmy722698@gmail.com>
2025-02-03 17:24:35 +0900

    Fix. resize 시 중간에서 refresh 할 경우 viewport 밖 섹션의 애니메이션이 중단되는 현상 발생 scrollTrigger refresh  계산은 위에서 아래로 되기 때문에 scroll 맨위로 위치시킨 후 refresh 하도록 변경

--- pmy/src/app/utils/hook/useLocoScroll.ts
+++ pmy/src/app/utils/hook/useLocoScroll.ts
@@ -7,6 +7,7 @@ import LocomotiveScroll from 'locomotive-scroll';
 import 'locomotive-scroll/src/locomotive-scroll.scss';
 import gsap from 'gsap';
 import * as animate from '@/utils/animate';
+import { debounce } from 'lodash';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -60,6 +61,24 @@ export default function useLocoScroll(start: boolean, ref: any) {
   const contact_tl = useRef<gsap.core.Timeline | null>(null);
 
   let timer;
+
+  /**
+   * resize 발생 시 고정핀 계산위해 scroll 처음으로 위치 시킨 후 refresh
+   * 중간에서 refresh 할 경우 viewport 밖의 timeline 멈춤.
+   */
+  useEffect(() => {
+    const handleResize = debounce(() => {
+      locoScrollRef.current?.scrollTo(0, { duration: 0 });
+      locoScrollRef.current?.update();
+      ScrollTrigger.refresh(); // 새로 계산
+    }, 300);
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   useEffect(() => {
     if (!ref.current) return;
