2275aeeb81e278e8b7dbe146844fb2ae55fdd16f
sia <parkmy722698@gmail.com>
2025-02-03 21:06:07 +0900

    Fix. type error

--- pmy/src/app/project/[id]/page.tsx
+++ pmy/src/app/project/[id]/page.tsx
@@ -1,7 +1,6 @@
-// /app/project/[id]/page.tsx
 import Project from '@/components/templates/Project';
 import { getPageWithBlocks } from '@/utils/service/notion';
-import { DatabaseKey } from '@/types/common';
+import { DatabaseKey, NotionRes } from '@/types/common';
 //서버에서 실행
 export default async function ProjectPage({
   params,
@@ -11,7 +10,14 @@ export default async function ProjectPage({
   const { id: pageId } = params; // params.id는 url 에서 추출된 동적 경로
 
   // 각 페이지의 블록 데이터 가져오기
-  const list = await getPageWithBlocks(pageId);
+  const result = await getPageWithBlocks(pageId);
+
+  // 비어있다면 빈객체 할당
+  const list: NotionRes =
+    result && 'pageWithBlocks' in result
+      ? result
+      : { pageWithBlocks: [], filtered: [] };
+
   return <Project list={list} />;
 }
 
