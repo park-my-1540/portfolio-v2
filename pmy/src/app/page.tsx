1590b5012379ab206b4d9ecd06eaa57f047fc121
sia <parkmy722698@gmail.com>
2025-02-15 01:20:52 +0900

    Fix.  cache: 'no-cache' 를 써야하기 때문에 notion.databases.query, notion.blocks.children.list fetch 로 변경

--- pmy/src/app/page.tsx
+++ pmy/src/app/page.tsx
@@ -1,6 +1,6 @@
 /**
  * @fileoverview Notion API 받아와서 page 컴포넌트에 전달
- * 서버사이드로 데이터 로드후 클라에서 렌더링 SSR
+ * SSR + SSG
  */
 import Page from '@/components/pages/index';
 import { getPageList } from '@/service/notion/notionService';
--- pmy/src/app/service/notion/notionClient.ts
+++ pmy/src/app/service/notion/notionClient.ts
@@ -6,18 +6,65 @@ import { TOKEN } from '@/../../config';
 import { DatabaseKey } from '@/types/common';
 const notion = new Client({ auth: TOKEN });
 
+const BASE_URL = 'https://api.notion.com/v1';
+
+// 데이터베이스 쿼리 요청
 export async function getDatabaseQuery(databaseId: DatabaseKey) {
-  return notion.databases.query({
-    database_id: databaseId,
-    sorts: [
-      {
-        property: 'duration',
-        direction: 'descending',
+  const url = `${BASE_URL}/databases/${databaseId}/query`;
+
+  try {
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+        'Content-Type': 'application/json',
+        'Notion-Version': '2022-06-28',
       },
-    ],
-  });
+      cache: 'no-cache', // no-cache 설정
+      body: JSON.stringify({
+        sorts: [
+          {
+            property: 'duration',
+            direction: 'descending',
+          },
+        ],
+      }),
+    });
+
+    if (!res.ok) {
+      throw new Error('Error fetching data');
+    }
+
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching database query:', error);
+    return null;
+  }
 }
 
+// 페이지 자식 블록 가져오기
 export async function getPageChildren(pageId: DatabaseKey) {
-  return notion.blocks.children.list({ block_id: pageId });
+  const url = `${BASE_URL}/blocks/${pageId}/children`;
+
+  try {
+    const res = await fetch(url, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+        'Notion-Version': '2022-06-28',
+      },
+      cache: 'no-cache', // no-cache 설정
+    });
+
+    if (!res.ok) {
+      throw new Error('Error fetching page children');
+    }
+
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching page children:', error);
+    return null;
+  }
 }
