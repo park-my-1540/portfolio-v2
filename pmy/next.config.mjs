0f6eee284cdcb8b2d21604356c11ed91feb6ee94
sia <parkmy722698@gmail.com>
2025-03-11 00:13:45 +0900

    첫 페이지 진입시 에러 X-Amz-Content-Sha256: UNSIGNED-PAYLOAD

--- pmy/next.config.mjs
+++ pmy/next.config.mjs
@@ -7,6 +7,13 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['prod-files-secure.s3.us-west-2.amazonaws.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
+        pathname: '/**',// 모든 경로 허용
+      },
+    ],
   },
 };
 
