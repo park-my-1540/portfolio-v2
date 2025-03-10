b5a0d8243583df83e7165a628cf27b56d5f41302
sia <parkmy722698@gmail.com>
2025-03-10 12:39:06 +0900

    notion router 변경

--- pmy/src/app/constants/routes.ts
+++ pmy/src/app/constants/routes.ts
@@ -1,6 +1,6 @@
 export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000';
 export const GIT = 'https://github.com/park-my-1540';
-export const NOTION = 'https://www.notion.so/0f834567330e47c6badc68700c5e96eb';
+export const NOTION = 'https://amusing-sparrow-b19.notion.site/1af927ab97618019894fc32e3b1a8a7a';
 
 // 페이지 경로
 export const ROUTES = {
